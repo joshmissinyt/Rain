41e36f0694fcb4cde3b3b9962fa241ad951b895e
joshmissinyt <joshmissinyt@gmail.com>
2019-05-12 01:35:34 +0100

    Added premake for project generation

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Rain"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-${cfg.architecture}"	
+
+project "Rain"
+	location "Rain"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rain/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RN_PLATFORM_WINDOWS",
+			"RN_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "RN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rain/vendor/spdlog/include",
+		"Rain/src"
+	}
+
+	links
+	{
+		"Rain"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"RN_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RN_DIST"
+		optimize "On"
