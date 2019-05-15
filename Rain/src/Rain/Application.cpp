92cdc8e2e0141147144d7d95074cd598a6308a0c
joshmissinyt <joshmissinyt@gmail.com>
2019-05-15 02:24:07 +0100

    Added window event support with GLFW.

--- Rain/src/Rain/Application.cpp
+++ Rain/src/Rain/Application.cpp
@@ -1,21 +1,31 @@
 #include "rnpch.h"
 #include "Application.h"
 
-#include "Rain/Events/ApplicationEvent.h"
 #include "Rain/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Rain {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		RN_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,5 +36,11 @@ namespace Rain {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
